53686308690badd9ba4993a14f60b2b00057c1fe
seongjo-Lee <55067691+lee0528kr@users.noreply.github.com>
2019-09-23 11:54:37 +0900

    Update 모의고사.java

--- 모의고사.java
+++ 모의고사.java
@@ -3,92 +3,95 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] answers) {
         
-        int[] a1=new int [answers.length]; // 수포자 1번이 답
-        int[] a2=new int [answers.length]; // 수포자 2번이 답
-        int[] a3=new int [answers.length]; // 수포자 3번의 답
-        int[] a22={1,3,4,5};
-        int score[]=new int[3];
-        int big=0,size=1;       
-        int[] a33={3,3,1,1,2,2,4,4,5,5};
-        int count=1,count2=0,count3=0;
+     
+        int[] a11={1,2,3,4,5};           // 수포자 1번이 찍는 유형
+        
+    
+        int[] a22={2,1,2,3,2,4,2,5};     // 수포자 2번이 찍는 유형
+        
+
+        int[] a33={3,3,1,1,2,2,4,4,5,5}; // 수포자 3번이 찍는 유형
+        
+        int score[]=new int[3]; // 수포자들의 점수를 담을 배열
+        int winner=0;  //가장 많이 맞춘사람을 담아둘 변수
+        int size=1; //answer 배열의 크기를 지정해 줄 변수       
+        
+        int count=0,count2=0,count3=0;
         
         for (int i=0; i<answers.length; i++)
         {
-           a1[i]=count; // 1 2 3 4 5 1 2 3 4 5 ...
-            count++;
-            if(count==6)
+            if(a11[count]==answers[i])
             {
-                count=1;
+                score[0]=score[0]+1;
+            }
+            count++;
+            if(count==5)
+            {
+                count=0;
             }
          }
         
         for (int i=0; i<answers.length; i++)
         {
-            if(i%2==0) // 0 2 4 6 8 등 2의 배수이면
-            {
-                a2[i]=2; // 2번답 기입
-            }
-            else
-            {
-                a2[i]=a22[count2]; // a22 의 n번째 배열을 기입 
+                if(a22[count2]==answers[i])
+                {
+                    score[1]=score[1]+1;
+                }
                 count2++;
-                if(count2==4)
+                if(count2==8)
                 {
                     count2=0;
                 }
-            }
          }
+        
         for (int i=0; i<answers.length; i++)
-        {
-            a3[i]=a33[count3]; // a33의 n번째 배열 기입
-            count3++;
+        {  
+            if(a33[count3]==answers[i])
+            {
+                score[2]=score[2]+1;
+            }
+             count3++;
             if(count3==10)
             {
                 count3=0;
             }
          }
-        //------------------------------답 기입 종료
-        for (int i=0; i<answers.length; i++)
-        {
-            if(answers[i]==a1[i])  // 1번째 수포자와 답이 같으면
-            {
-                score[0]=score[0]+1; // 1번째 수포자의 정답갯수1증가
-            }
-            if(answers[i]==a2[i])
-            {
-                score[1]=score[1]+1;
-            }
-            if(answers[i]==a3[i])
-            {
-                score[2]=score[2]+1;
-            }
-        }
-        
-        big=score[0]; // 1번째 수포자의 정답 갯수가 1등이라고 가정
+
+		 for(int s :score)
+			 System.out.println(s);
+                
+        winner=score[0]; // 1번째 수포자의 정답 갯수가 1등이라고 가정
         
         for (int i=1; i<3; i++) // 10 30 20   10 20 30    30 20 10  10 10 10   10 10 15
         {
-            if(big==score[i])
+
+			if(winner==score[i])   // 0 5 1
             {
-                size=size+1; // 배열의 크기만 증가시켜줌
+                size=size+1; // 배열의 크기만 증가시켜줌   
             }
-            
-            if(big<score[i]) // 가장 많이 맞춘사람 보다 높은 사람이나타나면
+
+			if(winner<score[i]) // 1번째 수포자보다 높은 사람이나타나면
             {
-                big=score[i];  // 높은 사람을 교체 해준다    
+                winner=score[i];  // 높은 사람을 교체 해준다    
                 size = 1;
             }
+
+            
         }
         
+		System.out.println(size);
         int[] answer = new int[size];
-        
+		
+        int j=0;
         for (int i=0; i<3; i++)
         {
-            if(big==score[i])
+            if(winner==score[i])
             {
-                answer[i]=i+1;// i+1 번째 사람이 가장 많이 맞춤
+                answer[j]=i+1;// i+1 번째 사람이 가장 많이 맞춤
+				j++;
             }
         }
+		// 
         
         Arrays.sort(answer);
         
