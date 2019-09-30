6f9719de0627370f5277015693386c687fe52319
seongjo-Lee <55067691+lee0528kr@users.noreply.github.com>
2019-09-30 23:11:05 +0900

    Add files via upload

---
+++ 같은숫자는싫어!.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution
+{
+   public int[] solution(int []arr)
+   {
+    final List<Integer> answer2 = new ArrayList<>();
+    int value = arr[0];
+    answer2.add(value);
+    for (int i = 1; i < arr.length; i++)
+    {
+        if (value != arr[i]) 
+        {
+            answer2.add(arr[i]);
+            value = arr[i];
+        }
+    }
+       
+      int[] answer = new int[answer2.size()];
+        
+        for(int k=0; k<answer2.size(); k++) {
+            answer[k] = answer2.get(k);
+        }
+       
+    return answer;
+   }
+}
